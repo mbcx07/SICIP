0fef2bc96eebc860822dafd32e9230fb54ef6ff9
Moisés Beltrán <moises.beltranx7@gmail.com>
2026-06-12 16:52:26 +0000

    v5.15.1: formato A4 y remover recepcion de pases

--- sicip-panels-v5.js
+++ sicip-panels-v5.js
@@ -1,5 +1,5 @@
 (function() {
-  var VERSION = "5.15";
+  var VERSION = "5.15.1";
 
   // ==================== GLOBAL DATA STORE ====================
   window.__SICIP_DATA__ = window.__SICIP_DATA__ || {};
